a317ac0c891182000d9e0010227a149689e04a9d
raji mk <raji.mukr@gmail.com>
2022-03-23 15:52:56 -0400

    eq data

---
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -0,0 +1,77 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with center and zoom level.
+// let map = L.map('mapid').setView([30, 30], 2);
+
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 50,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+// streets.addTo(map);
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    "Streets": streets,
+    "satellite": satelliteStreets
+  };
+
+  // Create the map object with a center and zoom level.
+let map = L.map("mapid", {
+    center: [39.5, -98.5],
+    zoom: 4
+  });
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+
+// Accessing the earthquake GeoJSON URL
+let earthquakeData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Grabbing our GeoJSON data.
+d3.json(earthquakeData).then(function(data) {
+    console.log(data);
+    // This function returns the style data for each of the earthquakes we plot on
+// the map. We pass the magnitude of the earthquake into a function
+// to calculate the radius.
+function styleInfo(feature) {
+    function getRadius(magnitude) {
+        if (magnitude === 0) {
+          return 1;
+        }
+        return magnitude * 4;
+      }
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: "#ffae42",
+      color: "#000000",
+      radius: getRadius(),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data,{
+    pointToLayer: function(feature, latlng) {
+        console.log(feature);
+        return L.circleMarker(latlng);
+  //       .bindPopup("<h2>" + "Magnitude" + features[0].properties.mag + "</h2> <hr> <h3> Place:" + features[0].properties.place + "</h3>");
+        },
+     style:styleInfo   
+  }).addTo(map);
+});
